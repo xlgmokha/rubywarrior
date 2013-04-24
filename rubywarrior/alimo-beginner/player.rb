8bd153ff53fa932505d11b34c18cca5b8d89c3c2
mo khan <mo@mokhan.ca>
2013-04-23 19:47:41 -0600

    no rest for the wicked

--- rubywarrior/alimo-beginner/player.rb
+++ rubywarrior/alimo-beginner/player.rb
@@ -1,9 +1,17 @@
 class Player
   def play_turn(warrior)
+    @initial_health ||= warrior.health
     if warrior.feel.empty?
       warrior.walk!
     else
+      warrior.rest! if health_is_low(warrior)
       warrior.attack!
     end
+  end
+
+  def health_is_low(warrior)
+    threshold = 0.5 * @initial_health
+    p "threshold is #{threshold}, initial health: #{@initial_health}, current: #{warrior.health}"
+    warrior.health <= threshold
   end
 end
--- rubywarrior/alimo-beginner/spec/player_spec.rb
+++ rubywarrior/alimo-beginner/spec/player_spec.rb
@@ -3,12 +3,13 @@ require 'ostruct'
 require_relative '../player.rb'
 
 describe Player do
+  let(:player) { Player.new }
+  let(:warrior) { fake }
+
   context "no obstacle" do
     let(:space) { OpenStruct.new(:empty? => true) }
 
     it "should walk forward" do
-      player = Player.new
-      warrior = fake
       warrior.stub(:feel).and_return(space)
       player.play_turn(warrior)
       warrior.should have_received(:walk!)
@@ -17,12 +18,27 @@ describe Player do
 
   context "with obstacle" do
     let(:space) { OpenStruct.new(:empty? => false) }
-    it "should attack obstacle" do
-      player = Player.new
-      warrior = fake
+    let(:starting_health) { 20 }
+    
+    before :each do
       warrior.stub(:feel).and_return(space)
+      warrior.stub(:health).and_return(starting_health)
+    end
+    it "should attack obstacle" do
       player.play_turn(warrior)
       warrior.should have_received(:attack!)
+    end
+    context "when our health reaches 50%" do
+      it "should step back and rest" do
+        player.play_turn(warrior)
+        warrior.should_not have_received(:rest!)
+
+        new_warrior = fake
+        new_warrior.stub(:health).and_return(starting_health * 0.5)
+        new_warrior.stub(:feel).and_return(space)
+        player.play_turn(new_warrior)
+        new_warrior.should have_received(:rest!)
+      end
     end
   end
 end

4d16156b2092db6868b133bcdf777d370f32be43
mo khan <mo@mokhan.ca>
2013-04-23 19:29:35 -0600

    complete level 2

--- rubywarrior/alimo-beginner/spec/player_spec.rb
+++ rubywarrior/alimo-beginner/spec/player_spec.rb
@@ -1,13 +1,28 @@
 require 'rspec-fakes'
+require 'ostruct'
 require_relative '../player.rb'
 
 describe Player do
-  context "when playing" do
+  context "no obstacle" do
+    let(:space) { OpenStruct.new(:empty? => true) }
+
     it "should walk forward" do
       player = Player.new
       warrior = fake
+      warrior.stub(:feel).and_return(space)
       player.play_turn(warrior)
       warrior.should have_received(:walk!)
+    end
+  end
+
+  context "with obstacle" do
+    let(:space) { OpenStruct.new(:empty? => false) }
+    it "should attack obstacle" do
+      player = Player.new
+      warrior = fake
+      warrior.stub(:feel).and_return(space)
+      player.play_turn(warrior)
+      warrior.should have_received(:attack!)
     end
   end
 end
